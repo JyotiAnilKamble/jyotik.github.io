1e6cb90da0614a7401f96fd0029b3c902bb9f137
JyotiAnilKamble <64472436+JyotiAnilKamble@users.noreply.github.com>
2020-11-19 01:06:03 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="stylesheet" href="button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+		
+	}
+	tr{
+		text-align:center;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("images/bank.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+
+	<div id="header">
+       <br>
+	   <br>
+	   <h1 style=" font-family:Comic Sans MS; font-size: 52px;color:#ff1493;text-shadow: 2px 2px black;"> The Sparks Foundation </h1>
+	   <h3 style=" font-family:Comic Sans MS; font-size: 30px;color:#ff1493;text-shadow: 2px 2px black;"> Designed by Ms.Jyoti Anil Kamble </h3>
+       <h2 style=" font-family:Times New Roman; font-size: 55px;color:#ff1493;text-shadow: 2px 2px black;"> Basic Banking System </h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="btn" type="button" href="getdetail.php">View users</button>
+                </a>
+                </tr>
+				
+			   <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="btn" type="button">All Transaction</button>
+               </a>
+               
+               </tr>
+
+            </table>
+    </div>
+</body>
+</html>
